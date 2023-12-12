4d88c4e532b8ba77b2bdceb071f97971fae9cadc
Marvin Gajek <Marvin.Gajek1@web.de>
2023-12-12 17:43:25 +0100

    update simple-go app

--- samples/simple-go/main.go
+++ samples/simple-go/main.go
@@ -1,21 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Request received:", r.Method, r.URL.Path)
-	fmt.Fprint(w, "Hello WASM from Go")
-}
+import "fmt"
 
 func main() {
-	http.HandleFunc("/", handler)
-	log.Println("Server started. Listening on :8080...")
-
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatalf("ListenAndServe error:%s ", err.Error())
-	}
+	fmt.Println("Hello WASM from Go")
 }
